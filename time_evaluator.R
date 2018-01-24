8a85d5d7e6a56b3afc21719cadf351210ab068c4
AikawaKai <marco.odore@studenti.unimi.it>
2018-01-24 03:43:35 +0100

    parallel version

--- time_evaluator.R
+++ time_evaluator.R
@@ -1,5 +1,9 @@
 library(caret)
-crossValidation <- function(number.folds, trainIndex, W, y_test, algorithm, Grid, string_class_matrix, test_set_list, model ){
+library(parallel)
+crossValidation <- function(number.folds, ntimes, trainIndex, W, y_test, algorithm, string_class_matrix){
+  library(caret)
+  AUROC <- AUPRC <- conf <- test_set_list <- model <- vector("list", ntimes);
+  exTime<-system.time(
   for(i in seq(1, number.folds)){
     tc <- trainControl(method = "cv", number = number.folds, classProbs = TRUE, summaryFunction = AUPRCSummary)
     set.seed(2);
@@ -31,12 +35,13 @@ crossValidation <- function(number.folds, trainIndex, W, y_test, algorithm, Grid
     #AUPRC[[i]] <- prSummary(test_set, lev = levels(test_set$obs));     
     #conf[[i]] <- best.threshold.confusion(test_set, thresholds = seq(0.01, 0.99, by=0.01));
     cat("End of iteration ", i, "\n");
-  }
+  })
+  write(algorithm, file=paste(datasetpath, algorithm, "_time.csv", sep = ""))
+  write(exTime[3], file=paste(datasetpath, algorithm, "_time.csv", sep = ""), append = TRUE)
 } 
 
 source("/home/kai/Documents/Unimi/Tesi-Bioinformatica/MachineLearning_ExecutionTimeEvaluation_R/metrics.R");
-args = commandArgs(trailingOnly=TRUE)
-print(args)
+
 datasetpath <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/"
 load(file=paste(datasetpath, "/6239_CAEEL/6239_CAEEL_STRING_NET_v10.5.rda", sep = ""))
 load(file=paste(datasetpath, "/6239_CAEEL/6239_CAEEL_GO_BP_ANN_STRING_v10.5_20DEC17.rda", sep=""))
@@ -47,22 +52,20 @@ W <- apply(W, FUN= function(x) x/1000, MARGIN = c(1,2))
 string_class_matrix<-cbind(W,y_test)
 ntimes <- 2
 number.folds = 2
-
-
-if(length(args)==0)
-{
-  algorithm="svmRadial"
-}else
-{
-  algorithm=args[1]
-}
+algorithms <- c("svmLinear", "svmRadial") #, "mlp", "mlpML", "AdaBoost.M1", "rf'")
 
 #Grid <-  expand.grid(C = c(.25, .5, 1), sigma = .05);
+no_cores <- detectCores() -1
+cl <- makeCluster(no_cores, errfile="./errParSeq.txt", outfile="./out.txt")
 
-
-AUROC <- AUPRC <- conf <- test_set_list <- model <- vector("list", ntimes);
+#vector("list", ntimes);
 set.seed(1)  # questo assicura che vengano create sempre le stesse partizioni
-trainIndex <- createFolds(factor(string_class_matrix[,nrow(string_class_matrix)]), k = number.folds, list = TRUE)
-exTime<-system.time(crossValidation(number.folds, trainIndex, W, y_test, algorithm, Grid, string_class_matrix, test_set_list, model))
-write(algorithm, file=paste(datasetpath, algorithm, "_time.csv", sep = ""))
-write(exTime[3], file=paste(datasetpath, algorithm, "_time.csv", sep = ""), append = TRUE)
+trainIndex <- createFolds(factor(string_class_matrix[,nrow(string_class_matrix)]), 
+                          k = number.folds, list = TRUE)
+clusterExport(cl, list("crossValidation", "number.folds", "ntimes",
+                       "trainControl", "AUPRCSummary", "y_test",
+                       "trainIndex", "W", "predict","compute.AUPRC",
+                       "evalmod", "datasetpath"))
+out<-parLapply(cl, algorithms, function(x) c(crossValidation(number.folds, ntimes, trainIndex, W, 
+                                                             y_test, x,
+                                                             string_class_matrix)))
