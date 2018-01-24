85df01d7e6361b0d68289ca4228e509305c15081
AikawaKai <marco.odore@studenti.unimi.it>
2018-01-24 01:31:40 +0100

    updated

--- time_evaluator.R
+++ time_evaluator.R
@@ -1,4 +1,40 @@
 library(caret)
+
+crossValidation <- function(number.folds, trainIndex, W, y_test, algorithm, Grid, string_class_matrix, test_set_list, model ){
+  for(i in seq(1, number.folds)){
+    tc <- trainControl(method = "cv", number = number.folds, classProbs = TRUE, summaryFunction = AUPRCSummary)
+    set.seed(2);
+    curr_y <- as.factor(y_test[trainIndex[[i]]])
+    curr_x <- as.data.frame(W[trainIndex[[i]],])
+    levels(curr_y)<-c("negative", "positive")
+    tryCatch({
+      model[[i]] <- caret::train(x=curr_x, y=curr_y,
+                                 method = algorithm, 
+                                 trControl = tc, 
+                                 verbose = FALSE, 
+                                 tuneGrid = Grid,
+                                 metric = "AUPRC")
+    },  error = function(err) {
+      print(err)
+    })
+    # Probabilistic prediction on the test set
+    model.prob <- predict(model[[i]], newdata = as.data.frame(W[-trainIndex[[i]],]), type = "prob")
+    # true labels
+    obs <- y_test[-trainIndex[[i]]]
+    # computing predicted labels at cutoff=0.5
+    pred <- factor(ifelse(model.prob$positive >= .5, "positive", "negative"), levels=c("positive","negative"));
+    # construction of the data frame for evaluating the predictions
+    test_set_list[[i]] <- data.frame(obs = obs, pred=pred, positive=model.prob$positive, negative=model.prob$negative); 
+    test_set <- test_set_list[[i]];
+    # computing AUROC
+    #AUROC[[i]] <- twoClassSummary(test_set, lev = levels(test_set$obs));  
+    # computing AUPRC
+    #AUPRC[[i]] <- prSummary(test_set, lev = levels(test_set$obs));     
+    #conf[[i]] <- best.threshold.confusion(test_set, thresholds = seq(0.01, 0.99, by=0.01));
+    cat("End of iteration ", i, "\n");
+  }
+} 
+
 source("/home/kai/Documents/Unimi/Tesi-Bioinformatica/MachineLearning_ExecutionTimeEvaluation_R/metrics.R");
 datasetpath <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/"
 load(file=paste(datasetpath, "/6239_CAEEL/6239_CAEEL_STRING_NET_v10.5.rda", sep = ""))
@@ -16,38 +52,6 @@ Grid <-  expand.grid(C=c(0.01));
 AUROC <- AUPRC <- conf <- test_set_list <- model <- vector("list", ntimes);
 set.seed(1)  # questo assicura che vengano create sempre le stesse partizioni
 trainIndex <- createFolds(factor(string_class_matrix[,nrow(string_class_matrix)]), k = number.folds, list = TRUE)
-View(caret::train)
-for(i in seq(1, number.folds)){
-  tc <- trainControl(method = "cv", number = number.folds, classProbs = TRUE, summaryFunction = AUPRCSummary)
-  set.seed(2);
-  curr_y <- as.factor(y_test[trainIndex[[i]]])
-  curr_x <- as.data.frame(W[trainIndex[[i]],])
-  levels(curr_y)<-c("negative", "positive")
-  tryCatch({
-      model[[i]] <- caret::train(x=curr_x, y=curr_y,
-                             method = algorithm, 
-                             trControl = tc, 
-                             verbose = FALSE, 
-                             tuneGrid = Grid,
-                             metric = "AUPRC")
-  },  error = function(err) {
-      print(err)
-                             })
-  # Probabilistic prediction on the test set
-  model.prob <- predict(model[[i]], newdata = as.data.frame(W[-trainIndex[[i]],]), type = "prob")
-  # true labels
-  obs <- y_test[-trainIndex[[i]]]
-  # computing predicted labels at cutoff=0.5
-  pred <- factor(ifelse(model.prob$positive >= .5, "positive", "negative"), levels=c("positive","negative"));
-  # construction of the data frame for evaluating the predictions
-  test_set_list[[i]] <- data.frame(obs = obs, pred=pred, positive=model.prob$positive, negative=model.prob$negative); 
-  test_set <- test_set_list[[i]];
-  # computing AUROC
-  #AUROC[[i]] <- twoClassSummary(test_set, lev = levels(test_set$obs));  
-  # computing AUPRC
-  #AUPRC[[i]] <- prSummary(test_set, lev = levels(test_set$obs));     
-  #conf[[i]] <- best.threshold.confusion(test_set, thresholds = seq(0.01, 0.99, by=0.01));
-  cat("End of iteration ", i, "\n");
-}
+crossValidation(number.folds, trainIndex, W, y_test, algorithm, Grid, string_class_matrix, test_set_list, model)
 
 
