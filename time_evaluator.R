8699062a4b262cc111ba897e0513a479323afd62
AikawaKai <marco.odore@studenti.unimi.it>
2018-01-26 13:42:28 +0100

    updated

--- time_evaluator.R
+++ time_evaluator.R
@@ -1,37 +1,38 @@
 library(caret)
+library(dplyr)
 library(parallel)
+#library(parallel)
 crossValidation <- function(number.folds, ntimes, trainIndex, W, y_test, algorithm){
   AUROC <- AUPRC <- conf <- test_set_list <- model <- vector("list", ntimes);
   #Grid <-  expand.grid(C=c(0.01,0.05, 0.1, 1, 10));
   for(i in seq(1, number.folds)){
     tc <- trainControl(method = "cv", number = number.folds, classProbs = TRUE, summaryFunction = AUPRCSummary)
     set.seed(2);
-    curr_y <- as.factor(y_test[trainIndex[[i]]])
-    curr_x <- as.data.frame(W[trainIndex[[i]],])
-    print(nrow(curr_x))
-    print(ncol(curr_x))
-    print(length(curr_y))
+    curr_y <- y_test[trainIndex[[i]]]
+    curr_y <- as.factor(curr_y)
     levels(curr_y)<-c("negative", "positive")
+    curr_x <- W[trainIndex[[i]],]
     if(length(unique(curr_y))<=1){
       print("empty")
       next
     }
-    model[[i]] <- caret::train(x=curr_x, y=curr_y,
-                                 method = algorithm, 
-                                 trControl = tc, 
-                                 #tuneGrid = Grid,
-                                 metric = "AUPRC")
+    model[[i]] <- caret::train(curr_x, curr_y,
+                               method = algorithm, 
+                               trControl = tc, 
+                               #tuneGrid = Grid,
+                               metric = "AUPRC")
+    
     
     # Probabilistic prediction on the test set
-    model.prob <- predict(model[[i]], newdata = as.data.frame(W[-trainIndex[[i]],]), type = "prob")
+    #model.prob <- predict(model[[i]], newdata = as.data.frame(W[-trainIndex[[i]],]), type = "prob")
 
     # true labels
-    obs <- y_test[-trainIndex[[i]]]
+    #obs <- y_test[-trainIndex[[i]]]
     # computing predicted labels at cutoff=0.5
-    pred <- factor(ifelse(model.prob$positive >= .5, "positive", "negative"), levels=c("positive","negative"));
+    #pred <- factor(ifelse(model.prob$positive >= .5, "positive", "negative"), levels=c("positive","negative"));
     # construction of the data frame for evaluating the predictions
-    test_set_list[[i]] <- data.frame(obs = obs, pred=pred, positive=model.prob$positive, negative=model.prob$negative); 
-    test_set <- test_set_list[[i]];
+    #test_set_list[[i]] <- data.frame(obs = obs, pred=pred, positive=model.prob$positive, negative=model.prob$negative); 
+    #test_set <- test_set_list[[i]];
     # computing AUROC
     #AUROC[[i]] <- twoClassSummary(test_set, lev = levels(test_set$obs));  
     # computing AUPRC
@@ -39,8 +40,8 @@ crossValidation <- function(number.folds, ntimes, trainIndex, W, y_test, algorit
     #conf[[i]] <- best.threshold.confusion(test_set, thresholds = seq(0.01, 0.99, by=0.01));
     cat("End of iteration ", i, "\n");
   }
-  write(algorithm, file=paste(datasetpath, algorithm, "_time.csv", sep = ""))
-  write(exTime[3], file=paste(datasetpath, algorithm, "_time.csv", sep = ""), append = TRUE)
+  #write(algorithm, file=paste(datasetpath, algorithm, "_time.csv", sep = ""))
+  #write(exTime[3], file=paste(datasetpath, algorithm, "_time.csv", sep = ""), append = TRUE)
 } 
 
 source("/home/kai/Documents/Unimi/Tesi-Bioinformatica/MachineLearning_ExecutionTimeEvaluation_R/metrics.R");
@@ -48,13 +49,13 @@ source("/home/kai/Documents/Unimi/Tesi-Bioinformatica/MachineLearning_ExecutionT
 datasetpath <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/"
 load(file=paste(datasetpath, "/6239_CAEEL/6239_CAEEL_STRING_NET_v10.5.rda", sep = ""))
 load(file=paste(datasetpath, "/6239_CAEEL/6239_CAEEL_GO_BP_ANN_STRING_v10.5_20DEC17.rda", sep=""))
-y_test <- ann[,1]
+y_test <- ann[,614]
 W <- W[seq(1,1000),]
 y_test <- y_test[seq(1,1000)]
 W <- apply(W, FUN= function(x) x/1000, MARGIN = c(1,2))
 ntimes <- 10
 number.folds <- 10
-algorithms <- c("svmLinear")#, "svmRadial") #, "mlp", "mlpML", 
+algorithms <- c("svmLinear", "svmRadial") #, "mlp", "mlpML", 
                 #"AdaBoost.M1", "rf", "C5.0", "xgbLinear",
                 #"lda", "LogitBoost", "gaussprPoly", "glmnet",
                 #"randomGLM", "treebag", "knn")
@@ -65,14 +66,12 @@ cl <- makeCluster(no_cores, errfile="./errParSeq.txt", outfile="./out.txt")
 
 #vector("list", ntimes);
 set.seed(1)  # questo assicura che vengano create sempre le stesse partizioni
-trainIndex <- caret::createFolds(factor(y_test), 
-                          k = number.folds, list = TRUE)
+trainIndex <- caret::createFolds(factor(y_test), k = number.folds, list = TRUE)
 
 clusterExport(cl, list("crossValidation", "number.folds", "ntimes",
                        "trainControl", "AUPRCSummary", "y_test",
                        "trainIndex", "W", "predict","compute.AUPRC",
                        "evalmod", "datasetpath"))
-#out<-parLapply(cl, algorithms, function(x) c(crossValidation(number.folds, ntimes, trainIndex, W, 
-                                                             #y_test, x,
-                                                             #string_class_matrix)))
-crossValidation(10, 10, trainIndex, W, y_test, "knn")
+out<-parLapply(cl, algorithms, function(x) c(crossValidation(number.folds, ntimes, trainIndex, W, 
+                                                             y_test, x)))
+#crossValidation(number.folds, number.folds, trainIndex, W, y_test, "svmLinear")
