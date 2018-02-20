040adf14e780ca23c679b027e0553982597a8c8c
AikawaKai <marco.odore@studenti.unimi.it>
2018-02-20 20:05:03 +0100

    updated

--- MLFeatureSelection/featureSelection.R
+++ MLFeatureSelection/featureSelection.R
@@ -10,9 +10,10 @@ ontologies <- c("/6239_CAEEL/6239_CAEEL_GO_BP_ANN_STRING_v10.5_20DEC17.rda",
                 "/6239_CAEEL/6239_CAEEL_GO_CC_ANN_STRING_v10.5_20DEC17.rda")
 
 ont_name <- c("BP", "MF", "CC")
-ont <- 1
+ont <- 2
 load(file=paste(datasetpath, ontologies[[ont]], sep = ""))
 
+W <- apply(W, FUN= function(x) x/1000, MARGIN = c(1,2))
 ann <- ann[,colSums(ann)>9]
 classes_names <- colnames(ann)
 fastIterTtest(ont_name[[ont]], W, ann, ptestCalculation, classes_names)
---
+++ MLFeatureSelection/plotHistogramsFromCsv.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import sys
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+
+
+if __name__ == '__main__':
+    file_name = sys.argv[1]
+    dataframe = pd.read_csv(file_name)
+    ind = dataframe.index
+    classes_ = []
+    vals = []
+    for i in ind:
+        class_ = dataframe.loc[i,:][0]
+        classes_.append(class_)
+        curr_row = dataframe.loc[i,:][1:]
+        count = len([float(v) for v in curr_row if float(v)<0.01])
+        vals.append(count)
+    print(min(vals))
+    print(max(vals))
+    n, bins, patches = plt.hist(vals, 20, edgecolor='black', facecolor='blue', alpha=0.7)
+    plt.show()
