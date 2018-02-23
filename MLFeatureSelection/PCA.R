84c4f66d178d67335b2da06a05e31d1db8dd8394
AikawaKai <marco.odore@studenti.unimi.it>
2018-02-23 16:42:41 +0100

    updated

--- MLFeatureSelection/PCA.R
+++ MLFeatureSelection/PCA.R
@@ -1,6 +1,11 @@
+library(factoextra)
 path_ <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/" #where to write csv
 datasetpath <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/" #where from extract data
 
 #STRING SIMILARITY MATRIX
 load(file=paste(datasetpath, "/6239_CAEEL/6239_CAEEL_STRING_NET_v10.5.rda", sep = ""))
-pca <- prcomp(W, scale. = TRUE)
+
+#W_test <- W[1:2000,]
+W_test <- apply(W_test, FUN= function(x) x/1000, MARGIN = c(1,2))
+pca <- prcomp(W_test)
+save(pca, file = "./pca.rda")
