80a3d58fb074fb261f771c83713e999bb5baa4a9
AikawaKai <marco.odore@studenti.unimi.it>
2018-02-23 15:04:59 +0100

    updated test

--- MLFeatureSelection/plotHistogramsFromCsv.py
+++ MLFeatureSelection/plotHistogramsFromCsv.py
@@ -16,12 +16,12 @@ if __name__ == '__main__':
         class_ = dataframe.loc[i,:][0]
         classes_.append(class_)
         curr_row = dataframe.loc[i,:][1:]
-        count = len([float(v) for v in curr_row if float(v)<0.01])
+        count = len([float(v) for v in curr_row if float(v)<0.01/(len(curr_row))])
         vals.append(count)
     print(min(vals))
     print(max(vals))
     n, bins, patches = plt.hist(vals, 20, edgecolor='black', facecolor='green', alpha=0.7)
-    plt.title(type_+" histogram\n p.value < 0.01")
+    plt.title(type_+" histogram\n p.value < (0.01 / tot. num. features)")
     plt.xlabel("Num. selected features")
     plt.ylabel("Num. classes")
-    plt.savefig(type_+".png")
+    plt.savefig("bonf_"+type_+".png")
