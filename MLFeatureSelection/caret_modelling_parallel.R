cc9b4145cd8cb7610b91036270f764ca255939d2
AikawaKai <marco.odore@studenti.unimi.it>
2018-02-24 22:14:13 +0100

    updated

--- MLFeatureSelection/caret_modelling_parallel.R
+++ MLFeatureSelection/caret_modelling_parallel.R
@@ -2,13 +2,18 @@ library(caret); 	## to model
 library(HEMDAG); 	## to create stratified fold (do.stratified.cv.data.single.class)
 
 #LIBS
+server.lib.dir <- "/home/modore/Tesi-Bioinformatica/BioinformaticThesis/MLFeatureSelection/lib/"
 lib.dir <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/BioinformaticThesis/MLFeatureSelection/lib/"
 source(paste(lib.dir, "caret.metrics.R", sep =""));  ## to use customize performance metrics ("precrec" pkg)
 source(paste(lib.dir, "R_CARET_MODELLING.R", sep = "")); ## to call the high-level fun caret.training
 
-## PARAMETERS SETTING
+## SERVER\LOCAL DATA PATH SETTING
+server.annotation.dir <- "/data/GO_EXP/"
+server.net.dir <- "/home/modore/"
 annotation.dir <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/";
 net.dir <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/BioinformaticThesis/MLFeatureSelection/data/"; ## set your data dir..
+
+## GENERAL PARAMETERS SETTING
 net.file <- "pca.rda";
 ann.file <- "/6239_CAEEL/6239_CAEEL_GO_MF_ANN_STRING_v10.5_20DEC17.rda";
 PreProc <- TRUE;
@@ -27,9 +32,9 @@ seed <- 1;
 # LOADING DATA
 load(file=paste(annotation.dir, ann.file, sep=""));
 load(file=paste(net.dir, net.file, sep=""));
-
 W <- pca$x
 
+#SELECT BY VARIANCE
 W <- W[,1:1000]
 
 # selected classes for replicability
