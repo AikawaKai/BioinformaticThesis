861f75fbc1273a55d1031168c3419099a62e35c3
AikawaKai <marco.odore@studenti.unimi.it>
2018-03-16 16:26:55 +0100

    updated

---
+++ MLFeatureSelection/results_analylis.R
@@ -0,0 +1,36 @@
+
+path_results <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/BioinformaticThesis/MLFeatureSelection/scores_variance/BP_results/BP_variance_70/variance_70_"
+ends <- "10fcv.rda"
+perf <- "perf"
+algorithms <- c("mlp", "xgbLinear", "svmLinear", "svmRadial", "glmnet", "C5.0", "lda",
+                "rf", "AdaBoost.M1", "LogitBoost", "gaussprPoly", "treebag", "knn")
+dataFrameAUC <- data.frame()
+dataFramePRC <- data.frame()
+index <- 1
+for(i in seq(1:length(algorithms))){
+  algo <- algorithms[[i]]
+  perfPath <- paste(path_results, algo, ".", perf, ".", ends, sep="")
+  tryCatch(
+    {
+      load(perfPath)
+      if(nrow(dataFrameAUC)==0){
+        dataFrameAUC <- data.frame(AUC.flat["per.class"])
+        dataFramePRC  <- data.frame(PRC.flat["per.class"])
+      }else{
+        dataFrameAUC <- cbind(dataFrameAUC, algo=AUC.flat["per.class"])
+        dataFramePRC <- cbind(dataFramePRC, algo=PRC.flat["per.class"])
+      }
+      print(algo)
+      names(dataFrameAUC)[index] <- algo
+      names(dataFramePRC)[index] <- algo
+      index = index + 1
+    }, error = function(err){
+       cat()
+      #print(perfPath)
+    })
+}
+dataFrameAUC
+dataFramePRC
+write.csv(dataFrameAUC, file=paste0(path_results,"auc.csv"))
+write.csv(dataFramePRC, file=paste0(path_results,"prc.csv"))
+
