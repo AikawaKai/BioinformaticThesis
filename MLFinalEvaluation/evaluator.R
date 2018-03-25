d7013b5fdfbbd7ea2e12aa820f985aab693d7463
AikawaKai <marco.odore@studenti.unimi.it>
2018-03-25 20:43:11 +0200

    updated

--- MLFinalEvaluation/evaluator.R
+++ MLFinalEvaluation/evaluator.R
@@ -2,6 +2,17 @@ library(caret);
 library(HEMDAG);
 
 SERVER <- FALSE
+args <- commandArgs(trailingOnly = TRUE)
+algorithm <- args[1]
+curr_onto <- args[2]
+type <- args[3]
+if(type=="FS"){
+  nfeature = 100
+}else if(type=="PCA"){
+  nfeature = 15
+}else{
+  stop("WRONG INPUT \nIf Feature Selection is needed type: FS \nIf pca is needed type: PCA")
+}
 
 if(SERVER){
   path <- "/home/modore/Documents/Unimi/Tesi-Bioinformatica/BioinformaticThesis/MLFinalEvaluation/"
