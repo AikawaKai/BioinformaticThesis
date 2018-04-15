d1404507c13c23c3c393df1b70c82fd41cb45e4d
AikawaKai <marco.odore@studenti.unimi.it>
2018-04-15 13:10:55 +0200

    updated

---
+++ DatasetStats/annotation_stats.R
@@ -0,0 +1,10 @@
+part1 <- "/home/kai/Documents/Unimi/Tesi-Bioinformatica/new_6239_CAEEL/6239_CAEEL_GO_"
+part2 <- "_ANN_STRING_v10.5_20DEC17.rda"
+ontos <- list("BP", "MF", "CC")
+for(onto in ontos){
+  curr_onto = paste0(part1, onto, part2)
+  print(curr_onto)
+  load(curr_onto)
+  ann <- ann[,colSums(ann)>9]
+  print(ncol(ann))
+}
