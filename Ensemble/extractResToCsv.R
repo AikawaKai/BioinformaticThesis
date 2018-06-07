b15c2bd3fb37071c8eda0bb750908bddc3c56348
AikawaKai <marco.odore@studenti.unimi.it>
2018-06-07 18:00:19 +0200

    updated

--- Ensemble/extractResToCsv.R
+++ Ensemble/extractResToCsv.R
@@ -1,4 +1,3 @@
-
 get_info_from_name <- function(file_name){
   res<-unlist(strsplit(file_name, "[.]"))
   onto <- res[7]
@@ -12,20 +11,24 @@ get_info_from_name <- function(file_name){
   if(algo=="C5"){
     algo = "C5.0"
   }
-  print("#######")
-  print(onto)
-  print(algo)
-  print(fs)
+  return(c(algo, fs, onto))
 }
 
-
-
 path <- "/home/kai/Documenti/UNIMI/BioinformaticThesis/Ensemble/hierPerf/"
-curr_method <- "GPAV"
+curr_method <- "GPAV/"
 path <- paste0(path, curr_method)
 files <- list.files(path = path, recursive = TRUE)
 files <- files[lapply(files, function(x){grepl(".rda", x)})==TRUE]
+list_ <- list(4)
 for(file in files){
-  get_info_from_name(file)
+  res <- get_info_from_name(file)
+  curr_file <- paste0(path, file)
+  load(curr_file)
+  PRC.flat <- get("PRC.flat")
+  PRC.hier <- get("PRC.hier")
+  AUC.flat <- get("AUC.flat")
+  AUC.hier <- get("AUC.hier")
+  curr_row <- c(res[1], PRC.flat[["average"]], PRC.hier[["average"]], AUC.flat[["average"]], AUC.hier[["average"]])
+  print(curr_row)
 }
 
