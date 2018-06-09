8628447a4626e1f3f4e50de35a22923f3044c157
AikawaKai <marco.odore@studenti.unimi.it>
2018-06-09 16:35:16 +0200

    updated

--- Ensemble/extractResToCsv.R
+++ Ensemble/extractResToCsv.R
@@ -14,6 +14,26 @@ get_info_from_name <- function(file_name){
   return(c(algo, fs, onto))
 }
 
+updateList <- function(list1, algo, fs, onto, AUC.flat, AUC.hier, PRC.flat, PRC.hier){
+  list1 <- data.frame(list1)
+  list1[,ncol(list1)+1] <- AUC.flat[["per.class"]]
+  names(list1)[ncol(list1)] <- paste0(algo, "_AUC.flat_", fs)
+  list1[,ncol(list1)+1] <- AUC.hier[["per.class"]]
+  names(list1)[ncol(list1)] <- paste0(algo, "_AUC.hier_", fs)
+  list1[,ncol(list1)+1] <- PRC.flat[["per.class"]]
+  names(list1)[ncol(list1)] <- paste0(algo, "_PRC.flat_", fs)
+  list1[,ncol(list1)+1] <- PRC.hier[["per.class"]]
+  names(list1)[ncol(list1)] <- paste0(algo, "_PRC.hier_", fs)
+  return(list1)
+}
+
+updateList1 <- function(list1, algo, fs, onto, FMM.flat, FMM.hier){
+  list1[,ncol(list1)+1] <- FMM.flat[["per.example"]][,4]
+  names(list1)[ncol(list1)] <- paste0(algo, "_FMM.flat_", fs)
+  list1[,ncol(list1)+1] <- FMM.hier[["per.example"]][,4]
+  names(list1)[ncol(list1)] <- paste0(algo, "_FMM.hier_", fs)
+  return(list1)
+}
 path <- "/home/kai/Documenti/UNIMI/BioinformaticThesis/Ensemble/hierPerf/"
 # curr_method <- "ISO-TPRthreshold.free"
 # curr_method <- "HTD"
@@ -28,8 +48,8 @@ bp_df <- data.frame(matrix(0, nrow = 1334))
 mf_df <- data.frame(matrix(0, nrow = 185))
 cc_df <- data.frame(matrix(0, nrow = 220))
 bp_df_fmm <- data.frame(matrix(0, nrow = 2597 ))
-mf_df_fmm <- data.frame(matrix(0, nrow = 185 ))
-cc_df_fmm <- data.frame(matrix(0, nrow = 2597 ))
+mf_df_fmm <- data.frame(matrix(0, nrow = 1806))
+cc_df_fmm <- data.frame(matrix(0, nrow = 1924 ))
 for(file in files){
   res <- get_info_from_name(file)
   curr_file <- paste0(path, file)
@@ -41,52 +61,19 @@ for(file in files){
   FMM.flat <- get("FMM.flat")
   FMM.hier <- get("FMM.hier")
   curr_row<- data.frame(res[1], res[2], res[3], PRC.flat[["average"]], PRC.hier[["average"]], 
-                AUC.flat[["average"]], AUC.hier[["average"]])
+                        AUC.flat[["average"]], AUC.hier[["average"]])
   names(curr_row)<-c("algo", "fs", "onto", "PRC.flat", "PRC.hier", "AUC.flat", "AUC.hier")
   to_write <- rbind(to_write, curr_row)
   
   if(res[[3]]=="BP"){
-    bp_df[,ncol(bp_df)+1] = AUC.flat[["per.class"]]
-    names(bp_df)[ncol(bp_df)] = paste0(res[1], "_AUC.flat_", res[[2]])
-    bp_df[,ncol(bp_df)+1] = AUC.hier[["per.class"]]
-    names(bp_df)[ncol(bp_df)] = paste0(res[1], "_AUC.hier_", res[[2]])
-    bp_df[,ncol(bp_df)+1] = PRC.flat[["per.class"]]
-    names(bp_df)[ncol(bp_df)] = paste0(res[1], "_PRC.flat_", res[[2]])
-    bp_df[,ncol(bp_df)+1] = PRC.hier[["per.class"]]
-    names(bp_df)[ncol(bp_df)] = paste0(res[1], "_PRC.hier_", res[[2]])
-    
-    #bp_df_fmm[,ncol(bp_df_fmm)+1] = FMM.flat[["per.example"]][["F"]]
-    #names(bp_df_fmm)[ncol(bp_df_fmm)] = paste0(res[1], "_FMM.flat_", res[[2]])
-    #bp_df_fmm[,ncol(bp_df_fmm)+1] = FMM.hier[["per.example"]][["F"]]
-    #names(bp_df_fmm)[ncol(bp_df_fmm)] = paste0(res[1], "_FMM.hier_", res[[2]])
+    bp_df <- updateList(bp_df, res[[1]], res[[2]], res[[3]], AUC.flat, AUC.hier, PRC.flat, PRC.hier)
+    bp_df_fmm <- updateList1(bp_df_fmm, res[[1]], res[[2]], res[[3]], FMM.flat, FMM.hier)
   }else if(res[[3]]=="MF"){
-    mf_df[,ncol(mf_df)+1] = AUC.flat[["per.class"]]
-    names(mf_df)[ncol(mf_df)] = paste0(res[1], "_AUC.flat_", res[[2]])
-    mf_df[,ncol(mf_df)+1] = AUC.hier[["per.class"]]
-    names(mf_df)[ncol(mf_df)] = paste0(res[1], "_AUC.hier_", res[[2]])
-    mf_df[,ncol(mf_df)+1] = PRC.flat[["per.class"]]
-    names(mf_df)[ncol(mf_df)] = paste0(res[1], "_PRC.flat_", res[[2]])
-    mf_df[,ncol(mf_df)+1] = PRC.hier[["per.class"]]
-    names(mf_df)[ncol(mf_df)] = paste0(res[1], "_PRC.hier_", res[[2]])
-    
-    #mf_df_fmm[,ncol(mf_df_fmm)+1] = FMM.flat[["per.example"]][["F"]]
-    #names(mf_df_fmm)[ncol(mf_df_fmm)] = paste0(res[1], "_FMM.flat_", res[[2]])
-    #mf_df_fmm[,ncol(mf_df_fmm)+1] = FMM.hier[["per.example"]][["F"]]
-    #names(mf_df_fmm)[ncol(mf_df_fmm)] = paste0(res[1], "_FMM.hier_", res[[2]])
+    mf_df <- updateList(mf_df, res[[1]], res[[2]], res[[3]], AUC.flat, AUC.hier, PRC.flat, PRC.hier)
+    mf_df_fmm <- updateList1(mf_df_fmm, res[[1]], res[[2]], res[[3]], FMM.flat, FMM.hier)
   }else{
-    cc_df[,ncol(cc_df)+1] = AUC.flat[["per.class"]]
-    names(cc_df)[ncol(cc_df)] = paste0(res[1], "_AUC.flat_", res[[2]])
-    cc_df[,ncol(cc_df)+1] = AUC.hier[["per.class"]]
-    names(cc_df)[ncol(cc_df)] = paste0(res[1], "_AUC.hier_", res[[2]])
-    cc_df[,ncol(cc_df)+1] = PRC.flat[["per.class"]]
-    names(cc_df)[ncol(cc_df)] = paste0(res[1], "_PRC.flat_", res[[2]])
-    cc_df[,ncol(cc_df)+1] = PRC.hier[["per.class"]]
-    names(cc_df)[ncol(cc_df)] = paste0(res[1], "_PRC.hier_", res[[2]])
-    
-    #cc_df_fmm[,ncol(cc_df_fmm)+1] = FMM.flat[["per.example"]][["F"]]
-    #names(cc_df_fmm)[ncol(cc_df_fmm)] = paste0(res[1], "_FMM.flat_", res[[2]])
-    #cc_df_fmm[,ncol(cc_df_fmm)+1] = FMM.hier[["per.example"]][["F"]]
-    #names(cc_df_fmm)[ncol(cc_df_fmm)] = paste0(res[1], "_FMM.hier_", res[[2]])
+    cc_df <- updateList(cc_df, res[[1]], res[[2]], res[[3]], AUC.flat, AUC.hier, PRC.flat, PRC.hier)
+    cc_df_fmm <- updateList1(cc_df_fmm, res[[1]], res[[2]], res[[3]], FMM.flat, FMM.hier)
   }
 }
 #print(to_write)
@@ -101,6 +88,6 @@ write.table(to_write, file = file_to_write, sep="\t", row.names = FALSE)
 write.table(bp_df[,2:ncol(bp_df)], file = file_to_writeBP, sep="\t", row.names = FALSE)
 write.table(mf_df[,2:ncol(mf_df)], file = file_to_writeMF, sep="\t", row.names = FALSE)
 write.table(cc_df[,2:ncol(cc_df)], file = file_to_writeCC, sep="\t", row.names = FALSE)
-write.table(bp_df_fmm, file = file_to_writeBP_FMM, sep="\t", row.names = FALSE)
-write.table(mf_df_fmm, file = file_to_writeMF_FMM, sep="\t", row.names = FALSE)
-write.table(cc_df_fmm, file = file_to_writeCC_FMM, sep="\t", row.names = FALSE)
+write.table(bp_df_fmm[,2:ncol(bp_df_fmm)], file = file_to_writeBP_FMM, sep="\t", row.names = FALSE)
+write.table(mf_df_fmm[,2:ncol(mf_df_fmm)], file = file_to_writeMF_FMM, sep="\t", row.names = FALSE)
+write.table(cc_df_fmm[,2:ncol(cc_df_fmm)], file = file_to_writeCC_FMM, sep="\t", row.names = FALSE)
